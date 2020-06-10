1421835de13a779971ce7335f2ce125adc3c85f2
Vishnu Dawah <vkdawah@gmail.com>
2020-06-10 11:19:51 -0400

    Handle failing of api request

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://7b14303cc7c2.ngrok.io",
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,31 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {
+  return async ({ email, password }) => {
     // make api request to sign up with email & password
+    try {
+      const response = await trackerApi.post("/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with sign up",
+      });
+    }
+
     // if we sign up, modify state  and say that we are authenticated
     // if sign up fails, we probably need to reflect an error message somewhere
   };
@@ -32,5 +48,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: "" }
 );
