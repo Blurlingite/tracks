519e36e155050b9e28edf19ca78f3597cfa076cb
Vishnu Dawah <vkdawah@gmail.com>
2020-06-11 19:26:14 -0400

    Can now signout of app

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://3f156b05846d.ngrok.io",
+  baseURL: "http://5ce38dfdeeb8.ngrok.io",
 });
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
       return { errorMessage: "", token: action.payload };
     case "clear_error_message":
       return { ...state, errorMessage: "" };
+    case "signout":
+      return { token: null, errorMessage: "" };
     default:
       return state;
   }
@@ -75,10 +77,10 @@ const signin = (dispatch) => async ({ email, password }) => {
   // Handle failure by showing error message
 };
 
-const signout = (dispatch) => {
-  return () => {
-    // signout
-  };
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem("token");
+  dispatch({ type: "signout" });
+  navigate("loginFlow");
 };
 
 export const { Provider, Context } = createDataContext(
